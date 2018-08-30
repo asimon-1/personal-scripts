a3c500d4527c45b119cad1bba2857eb88f401bf4
Andrew Simon <asimon@domeng.com>
2018-08-30 14:07:58 -0400

    AOC Day 9 part 1 solved

--- adventofcode/2017_day_9.py
+++ adventofcode/2017_day_9.py
@@ -2,17 +2,28 @@ import os
 import re
 import sys
 
+# From https://stackoverflow.com/a/4285211
+def parenthetic_contents(string):
+    """Generate parenthesized contents in string as pairs (level, contents)."""
+    stack = []
+    for i, c in enumerate(string):
+        if c == '{':
+            stack.append(i)
+        elif c == '}' and stack:
+            start = stack.pop()
+            yield (len(stack) + 1, string[start + 1: i])
+
 # Get the base directory. Input file must be in the same directory.
 BASE_DIR = os.path.dirname(sys.argv[0])
 
 # Read the input file
-with open(BASE_DIR + '\\' + '2017_day_9_input_test.txt') as f:
+with open(BASE_DIR + '\\' + '2017_day_9_input.txt') as f:
     raw = f.read()
 
 input_stream = [raw]
 
 # Remove negated characters
-pattern = r"!." # Note that double exclaimation points are removed left-to-right, so !!a means the a is not negated.
+pattern = r"!."  # Note that double exclaimation points are removed left-to-right, so !!a means the a is not negated.
 regex_negated = re.compile(pattern)
 input_stream.append(regex_negated.sub('', input_stream[-1]))
 
@@ -27,9 +38,7 @@ regex_other = re.compile(pattern)
 input_stream.append(regex_other.sub('', input_stream[-1]))
 
 # Count groups
-pattern = r"\{.*?\}"
-regex_group = re.compile(pattern)
-# How do I grab all of the outer groups?
+input_stream.append(list(parenthetic_contents(input_stream[-1])))
+score = sum([i[0] for i in input_stream[-1]])
 
-print(raw)
-for i in input_stream: print(i)
+print('Score: {}'.format(score))
