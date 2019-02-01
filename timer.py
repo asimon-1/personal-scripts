66f1ce11f6542b00bac9bbc12561248d7faebffc
Andrew Simon <asimon@domeng.com>
2019-02-01 08:46:03 -0500

    Simple timer app

---
+++ timer.py
@@ -0,0 +1,19 @@
+import datetime
+import PySimpleGUI as sg
+
+
+def tick():
+    '''Returns the current date and time as a string'''
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+layout = [[sg.Text(tick(), key='Time')]]
+window = sg.Window('', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
+
+while True:
+    event, values = window.Read(timeout=2)
+    if event is None or event == 'Exit':  # Ends the script if the window is closed
+        break
+    window.FindElement('Time').Update(tick())  # Update the displayed text
+window.Close()
