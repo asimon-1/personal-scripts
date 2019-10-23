c68b13f68b6e1e000d10f84ef20dac95010b7be1
Andrew Simon <asimon@domeng.com>
2019-10-23 14:34:50 -0400

    Try committing unformatted python file

---
+++ create-file-with-size.py
@@ -0,0 +1,26 @@
+"""Create a file with the given size.
+
+Disk usage is actually low even for large files since only a single byte is written.
+
+"""
+
+
+def badfunc(*args):
+    sum(args)
+    return
+
+
+if __name__ == "__main__":
+    units = {"B": 1, "KB": 1024, "MB": 1024 * 1024, "GB": 1024 * 1024 * 1024}
+
+    Raw = input("Enter the desired size of file (e.g. 10 MB):\t")  # Bad Comment
+
+    size, unit = Raw.split()
+    size = int(size)
+    filename = f"{size}_{unit.upper()}.bin"
+    unit = units[unit.upper()]
+
+    with open(filename, "wb") as f:
+        f.seek(size * unit - 1)
+        f.write(b"\0")
+    print(f"Created file: {filename}")
